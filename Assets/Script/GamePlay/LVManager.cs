5abfbde402dc691512602d02168979fdabac7483
alals93vu6 <a0987915131@gmail.com>
2022-05-23 04:41:45 +0800

    Add PlayerLV And EnemyLV Demo

---
+++ Assets/Script/GamePlay/LVManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LVManager : MonoBehaviour
+{
+    [Header("數值")] 
+    [SerializeField] public int EnemyHPLV;
+    [SerializeField] public int PlayerHPLV;
+    [SerializeField] public int NowLV;
+    [SerializeField] public int NowHPLV;
+    [SerializeField] public int NowPowerLV;
+
+    private UIManager UICtrl;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UICtrl = GetComponent<UIManager>();
+        NowLV = PlayerPrefs.GetInt("NowGameLV");
+        NowHPLV = PlayerPrefs.GetInt("NowPlayerHPLV");
+        NowPowerLV = PlayerPrefs.GetInt("NowPlayerPowerLV");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void PlayerHPSet()
+    {
+        
+    }
+}
