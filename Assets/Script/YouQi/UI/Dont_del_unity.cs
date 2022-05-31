4d7fb89dfd2d1eaed42ee4f219dffa10786b09aa
YouQi <alexjohn2109a@gmail.com>
2022-06-01 00:19:00 +0800

    Fix_UI

--- Assets/Script/YouQi/UI/Dont_del_unity.cs
+++ Assets/Script/YouQi/UI/Dont_del_unity.cs
@@ -6,10 +6,10 @@ public class Dont_del_unity : MonoBehaviour
 {
     // Start is called before the first frame update
     public int Page_now_int = 1;
-    
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
