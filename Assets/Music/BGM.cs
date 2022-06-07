81d8722cada942fe5859d56e89addff3ed189c92
YouQi <alexjohn2109a@gmail.com>
2022-06-07 23:06:10 +0800

    Update_Sound & Add_BGM

---
+++ Assets/Music/BGM.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    public AudioSource bgm_music;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgm_music.UnPause();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgm_music.UnPause();
+    }
+}
--- Assets/Music/Music.cs
+++ Assets/Music/Music.cs
@@ -16,5 +16,17 @@ public class Music : MonoBehaviour
     void Update()
     {
         DontDestroyOnLoad(this.gameObject);
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Instantiate(Sound_Attack, this.transform.position, new Quaternion(0, 0, 0, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Instantiate(Sound_Attack1, this.transform.position, new Quaternion(0, 0, 0, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Instantiate(Sound_Def, this.transform.position, new Quaternion(0, 0, 0, 0));
+        }
     }
 }
