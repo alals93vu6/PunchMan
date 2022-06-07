ae237ac3a902d0d7f03fd2c92d3a36b8b0e97a8c
alals93vu6 <a0987915131@gmail.com>
2022-06-07 22:15:09 +0800

    OWO

---
+++ Assets/Script/Events/Gym/PlayerLVUP.cs
@@ -0,0 +1,7 @@
+﻿namespace Project.Gym
+{
+    public class PlayerLVUP
+    {
+        
+    }
+}
--- Assets/Script/GameGym/GymUIManager.cs
+++ Assets/Script/GameGym/GymUIManager.cs
@@ -5,15 +5,16 @@ using UnityEngine.UI;
 
 public class GymUIManager : MonoBehaviour
 {
+    [SerializeField] public GymLVManager GymLV;
+    
     [SerializeField] private Text GymButtonText;
     [SerializeField] private Text PowerNowLV;
-    [SerializeField] private Text NeedMoney;
-    [SerializeField] private Text NeedTime;
-    
+    [SerializeField] private Text NeedMoneyAndTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GymLV = GetComponent<GymLVManager>();
     }
 
     // Update is called once per frame
@@ -21,6 +22,26 @@ public class GymUIManager : MonoBehaviour
     {
         
     }
-    
-    
+
+    public void ReadyUP()
+    {
+        GymButtonText.text = "開始升級";
+        PowerNowLV.text = "現在等級：" + GymLV.PlayerLV;
+        NeedMoneyAndTime.text = "升級所需金錢：" + GymLV.PlayerNeedMoney;
+        
+    }
+
+    public void UPing()
+    {
+        GymButtonText.text = "快進過程";
+        PowerNowLV.text = "現在等級：" + GymLV.PlayerLV;
+        
+    }
+
+    public void MoneyChanges()
+    {
+        NeedMoneyAndTime.text = "完成時間剩餘：" + GymLV.PlayerNeedTime;
+    }
+
+
 }
