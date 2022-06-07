c2f885976abed1a2d4aac952730d9741653c6147
alals93vu6 <a0987915131@gmail.com>
2022-06-08 00:22:30 +0800

    Enemy Animator

--- Assets/Script/GamePlay/EnemyAnimatorManager.cs
+++ Assets/Script/GamePlay/EnemyAnimatorManager.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class EnemyAnimatorManager : MonoBehaviour
 {
+    #region Instance
+    static public EnemyAnimatorManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+    
+    [Header("Other")] 
+    [SerializeField] private Animator EnemyAN;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        EnemyAN = GetComponent<Animator>();
     }
 
     // Update is called once per frame
