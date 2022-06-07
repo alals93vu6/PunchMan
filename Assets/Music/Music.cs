f921cc4e991dbe53d4fd9f45b88ad07b42bbec77
YouQi <alexjohn2109a@gmail.com>
2022-06-07 22:15:48 +0800

    Add_Attack & Def _Sound

---
+++ Assets/Music/Music.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource Sound_Attack;
+    public AudioSource Sound_Attack1;
+    public AudioSource Sound_Def;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
