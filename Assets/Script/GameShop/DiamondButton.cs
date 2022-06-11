dae8318d2e9caea82109706f39af16d2126a548e
alals93vu6 <a0987915131@gmail.com>
2022-06-11 23:33:41 +0800

    Add Buy Diamond

---
+++ Assets/Script/GameShop/DiamondButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondButton : MonoBehaviour
+{
+    public int AddNumber;
+
+    [SerializeField] private int PlayerHaveDiamond;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerHaveDiamond = PlayerPrefs.GetInt("PlayerDiamond");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BuyDiamond()
+    {
+        PlayerHaveDiamond = PlayerPrefs.GetInt("PlayerDiamond");
+
+        PlayerHaveDiamond = PlayerHaveDiamond + AddNumber;
+        PlayerPrefs.SetInt("PlayerDiamond",PlayerHaveDiamond);
+
+    }
+}
