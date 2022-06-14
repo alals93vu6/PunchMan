1281cd5f6370e1369b878656dfa8b2a436bf606d
alals93vu6 <a0987915131@gmail.com>
2022-06-14 17:04:50 +0800

    Add Shop Buy Money

---
+++ Assets/Script/GameShop/MoneyBuyButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyBuyButton : MonoBehaviour
+{
+    public int AddNumber;
+    public int NeedNumber;
+
+    [SerializeField] private int PlayerHaveMoney;
+    [SerializeField] private int PlayerHaveDiamond;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerHaveDiamond = PlayerPrefs.GetInt("PlayerDiamond");
+        PlayerHaveMoney = PlayerPrefs.GetInt("PlayerMoney");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BuyMoney()
+    {
+        PlayerHaveDiamond = PlayerPrefs.GetInt("PlayerDiamond");
+        PlayerHaveMoney = PlayerPrefs.GetInt("PlayerMoney");
+        
+        PlayerHaveDiamond = PlayerHaveDiamond - NeedNumber;
+        PlayerHaveMoney = PlayerHaveMoney + AddNumber;
+        
+        PlayerPrefs.SetInt("PlayerDiamond",PlayerHaveDiamond);
+        PlayerPrefs.SetInt("PlayerMoney",PlayerHaveMoney);
+
+    }
+}
--- Assets/Script/GameShop/ShopUIManager.cs
+++ Assets/Script/GameShop/ShopUIManager.cs
@@ -5,45 +5,60 @@ using UnityEngine.UI;
 
 public class ShopUIManager : MonoBehaviour
 {
-    [SerializeField] private bool IsGlovePage;
+    [SerializeField] private int IsGlovePage;
 
     [SerializeField] public GameObject GloveUI;
     [SerializeField] public GameObject DiamondUI;
+    [SerializeField] public GameObject MoneyUI;
 
     [SerializeField] public Text ChangeButtonText;
     
     // Start is called before the first frame update
     void Start()
     {
-        IsGlovePage = true;
+        IsGlovePage = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IsGlovePage)
+        if (IsGlovePage == 1)
         {
             GloveUI.SetActive(true);
             DiamondUI.SetActive(false);
+            MoneyUI.SetActive(false);
             ChangeButtonText.text = "購買鑽石";
         }
-        else
+        if(IsGlovePage == 2)
         {
             GloveUI.SetActive(false);
             DiamondUI.SetActive(true);
+            MoneyUI.SetActive(false);
+            ChangeButtonText.text = "購買金幣";
+        }
+        if(IsGlovePage == 3)
+        {
+            GloveUI.SetActive(false);
+            DiamondUI.SetActive(false);
+            MoneyUI.SetActive(transform);
             ChangeButtonText.text = "購買拳套";
         }
     }
 
     public void ChangePageButton()
     {
-        if (IsGlovePage)
+        if (IsGlovePage == 1)
         {
-            IsGlovePage = false;
+            IsGlovePage = 2;
         }
-        else
+        if (IsGlovePage == 2)
         {
-            IsGlovePage = true;
+            IsGlovePage = 3;
         }
+        if (IsGlovePage == 3)
+        {
+            IsGlovePage = 1;
+        }
+        
     }
 }
